64ef29346da547030643d1f01a24546a1e0becbe
[Ali Yilmaz] <[aliyilmaztr96@gmail.com]>
2023-03-01 23:33:46 +0300

    Recursive-ile-Fibonacci-Serisi-Bulan-Program

--- src/RecursiveIleFibonacciSerisiBulanProgram.java
+++ src/RecursiveIleFibonacciSerisiBulanProgram.java
@@ -1,10 +1,4 @@
-# Recursive-ile-Fibonacci-Serisi-Bulan-Program
-Java Recursive ile Fibonacci Serisi Bulan Program
-
-www.patika.dev
-
 public class Main {
-    
     static int fib(int n) {
         if (n == 1 || n == 2) {
             return 1;
